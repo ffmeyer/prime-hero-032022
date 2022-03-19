5f1b26becbd307f1c74022fd6125b00fec2b0d95
Felipe Meyer <ffmeyer@gmail.com>
2022-03-19 10:07:08 -0300

    include robot files

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ elif [[ $# == 1 ]]; then
 elif [[ $# == 2 ]]; then
     robot -d ./tests/logs/ -i ${2} tests/features/${1}.robot
 else
-    echo '\nno parameters -> run all robo using logs folder'
-    echo '\n1  parameter  -> run specific "${PARAM1}".robot'
-    echo '\n2  parameters -> run specific "${PARAM1}".robot and specific Tag "${PARAM2}"'
+    printf  '\nNo parameters -> run all robot files using tests / logs directory'
+    printf  '\nOne  parameter  -> run specific "${PARAM1}".robot'
+    printf  '\nTwo  parameters -> run specific "${PARAM1}".robot and specific Tag "${PARAM2}"'
 fi
